ab9c8d1c597b9dab2ed9f3fe7c2c05ba86c84b20
usman1486 <usmanakhtar1486@gmail.com>
2022-07-13 00:35:45 +0500

    Simple_Greetings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM from "react-dom";
+import "./index.css";
+let tt=new Date(2022,5,5,17);
+ let newtime=tt.getHours();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let greetings="";
+let bG={};
+if(newtime>=0 &&  newtime<12)
+{
+greetings="Good Morning";
+bG.color="green";
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+else if(newtime>=12 &&  newtime<=16)
+{
+greetings="Good Noon";
+bG.color="orange";
+}
+
+
+else if(newtime>16 &&  newtime<=18)
+{
+greetings="Good Evening";
+bG.color="pink";
+
+}
+else
+{
+  
+
+greetings="Good Night";
+bG.color="white";
+bG.backgroundColor="black";
+
+}
+
+
+
+ReactDOM.render(
+  <>
+<h1>HELLO SIR! <span style={bG}>{greetings} </span></h1>
+
+</>
+
+,document.getElementById("root"));
+
